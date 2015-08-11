eda7d2dd488e6d1c902b8552cf2bb331fe652719
Admin <admin@Admins-iMac.local>
2015-08-11 03:06:08 -0700

    Initial Commit

---
+++ iWeather/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iWeather
+//
+//  Created by Admin on 8/11/15.
+//  Copyright (c) 2015 Nguyen Duy Khanh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iWeather/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iWeather
+//
+//  Created by Admin on 8/11/15.
+//  Copyright (c) 2015 Nguyen Duy Khanh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
